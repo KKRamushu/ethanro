9a79420601a19d7b69162717d29e0e99305593e4
KK <kg.ramushu@gmail.com>
2024-08-15 21:10:34 +0200

    2nd commit

--- ethanro/store/models.py
+++ ethanro/store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+  imageUrl = models.CharField(max_length=1200)
+  title = models.CharField(max_length=64)
+  discription = models.CharField(max_length=250)
+  price = models.FloatField(max_length=8)
+  def __str__(self):
+      return f"{self.title}"
--- ethanro/store/views.py
+++ ethanro/store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.db import IntegrityError
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+  products = Product.objects.all()
+  return render(request, "store/index.html",{"products":products})
